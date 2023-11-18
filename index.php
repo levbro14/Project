4d6c8fdedd49dcc5ec2df74e94e4721422322970
levbro14 <levletsplay@gmail.com>
2023-11-18 14:32:24 +0700

    Create Site

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<head>
+    
+    <title>Это тестовый сайт для загрузки игры</title>
+    <link rel="icon" type="image/png" href="/Icon1.PNG">
+    <body background="Background.PNG">
+    <footer>&copy; 2023 Сайт создан в ознакомительных целях!</footer>
+        
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em;
+        }
+
+        main {
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            margin: 10px;
+            cursor: pointer;
+            background-color: #333;
+            color: #fff;
+            border: none;
+            border-radius: 10px;
+            
+            margin-left: 500px;
+            margin-top: 250px;
+        }
+
+        button:hover {
+            background-color: #555;
+        }
+
+        footer {
+            text-align: center;
+            padding: 10px;
+            background-color: #333;
+            color: #fff;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+        div {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 32px;
+            margin: 10px;
+            color: #40E0D0;
+            border: none;
+            border-radius: 5px;
+            
+            margin-left: 350px;
+            margin-top: 100px;
+        }
+        
+        
+        .UpdateButton {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            margin: 10px;
+            cursor: pointer;
+            background-color: #333;
+            color: #fff;
+            border: none;
+            border-radius: 10px;
+            
+            margin-left: 550px;
+            margin-top: 100px;
+        }
+        
+    </style>
+    
+    
+</head>
+<body>
+    <a href="GameFiles.rar" download=""><button>Ссылка на загрузку Version: 1.0</button></a>
+    <div>Обо мне: я один разработчик этой игры</div>
+
+    <button OnClick="window.location.href = 'index.php'" class="UpdateButton">Обновить сайт</button>
+    
+    <div> </div>
+    <div> </div>
+    <div> </div>
+    <div> </div>
+
+
+</body>
+</html>
